ef689c383f562d1f035cf63106b5bdccc75ebbb2
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-01-23 18:48:22 +0530

    creating api-gateway

---
+++ api-gateway/src/test/java/in/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
