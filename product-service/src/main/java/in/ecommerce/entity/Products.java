5b73365f271723ecf62b4c6e1436b121e776e93f
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-01-31 17:08:10 +0530

    adding toString but removed

--- product-service/src/main/java/in/ecommerce/entity/Products.java
+++ product-service/src/main/java/in/ecommerce/entity/Products.java
@@ -33,4 +33,72 @@ public class Products {
 
     @Column(nullable = false)
     private Boolean active = true;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "Products [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", quantity=" + quantity + ", category=" + category + ", active=" + active + "]";
+	}
+	
+	
+	
+	
+    
+    
 }
