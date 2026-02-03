7380aa089bd5f96f782d658abadd58bb07214d8b
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-02-03 19:44:57 +0530

    controller

--- order-service/src/main/java/in/ecommerce/controller/OrderController.java
+++ order-service/src/main/java/in/ecommerce/controller/OrderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,11 +26,13 @@ public class OrderController {
 	@Autowired
 	private OrderService service;
 
+	@PreAuthorize(value = "hasRole('USER')")
 	@PostMapping
 	public ResponseEntity<OrderRespose> placeOrder(@RequestBody OrderRequest request) {
 		return ResponseEntity.ok(service.orderPlaced(request));
 	}
 
+	@PreAuthorize(value = "hasRole('USER')")
 	@GetMapping("/user/{userId}")
 	public ResponseEntity<List<Order>> getOrdersByUser(@PathVariable Long userId) {
 
