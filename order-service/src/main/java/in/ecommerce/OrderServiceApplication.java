28ac9375faac16728aeb72fa1478fa9b850df547
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-02-03 19:48:08 +0530

    starting class

--- order-service/src/main/java/in/ecommerce/OrderServiceApplication.java
+++ order-service/src/main/java/in/ecommerce/OrderServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "in.ecommerce.client")
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
