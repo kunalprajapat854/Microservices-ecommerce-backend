6da5daeb10a45b57118278e7c769dae3d0f90c09
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-02-05 23:31:46 +0530

    swagger OpenApi config class

---
+++ user-service/src/main/java/in/ecommerce/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package in.ecommerce.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "E-Commerce API",
+        version = "1.0",
+        description = "Microservice APIs"
+    ),
+    security = @SecurityRequirement(name = "bearerAuth"),
+    servers = {
+        @Server(url = "http://localhost:8088", description = "API Gateway")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
